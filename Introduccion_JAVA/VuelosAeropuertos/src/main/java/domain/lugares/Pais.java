b1e2c6d530f4bbe56c979c5e36ee471977d104a6
JoshuaMorales99 <jmorales@frba.utn.edu.ar>
2026-04-18 20:49:02 -0300

    Elimino imports y constructor innecesario

--- Introduccion_JAVA/VuelosAeropuertos/src/main/java/domain/lugares/Pais.java
+++ Introduccion_JAVA/VuelosAeropuertos/src/main/java/domain/lugares/Pais.java
@@ -1,7 +1,5 @@
 package domain.lugares;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Pais {
@@ -10,10 +8,6 @@ public class Pais {
     private Continente continente;
     private List<Ciudad> ciudades;
 
-    // 📌 CONSTRUCTOR
-    public Pais() {
-        this.ciudades = new ArrayList<>();
-    }
 
     // 📌 GETTERS & SETTERS
     public String getNombre() {
